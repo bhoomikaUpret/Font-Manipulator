f74b9f2389361c0632a37407af9b12b305a59cba
bhoomikaUpret <112538577+bhoomikaUpret@users.noreply.github.com>
2023-05-02 20:17:07 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
+leftWristX = 0;
+rightWristX = 0;
+difference = 0;
+
 function setup()
 {
     video = createCapture(VIDEO);
-    video.size(500, 500);
+    video.size(550, 500);
 
     canvas = createCanvas(450, 450);
-    canvas.position(760, 100);
+    canvas.position(770, 130);
 
     poseNet = ml5.poseNet(video , modelLoaded);
     poseNet.on('pose', gotPoses);
@@ -12,7 +16,11 @@ function setup()
 
 function draw()
 {
-    background('#969A97')
+    background('#969A97');
+    textSize(difference);
+    text('Bhoomika' , 0 , 230);
+    fill('#F90093');
+    stroke('#F90093');
 }
 
 function modelLoaded()
@@ -20,10 +28,16 @@ function modelLoaded()
     console.log("PoseNet Is Initialized");
 }
 
-function gotPoses()
+function gotPoses(results)
 {
-    if(result.length > 0)
+    if(results.length > 0)
     {
         console.log(results);
+
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference = floor(leftWristX - rightWristX);
+
+        console.log("leftWristX = " + leftWristX + " rightwristX = " + rightWristX + " difference = " + difference)
     }
 }
